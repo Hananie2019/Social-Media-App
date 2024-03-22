a647f6d57314daec1f498c6b15218070f55d12ae
Hananie2019 <60793648+Hananie2019@users.noreply.github.com>
2024-03-22 17:02:46 -0400

    Passed props to Post component

--- web-projects/social-media-app/src/App.jsx
+++ web-projects/social-media-app/src/App.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 import Post from './components.js/Post.jsx'; // Correct import path
 
 
-function App() {
-    return (
-        <div>
-            <h1>Fakebook!</h1>
-            <Post /> {/* Render the Post component */}
-        </div>
-    );
+function App(){ 
+  return(
+    <div>
+      <h1>Fakebook!</h1>
+      
+        {/* CreatePostForm */}
+        
+        {/* Feed */}
+        
+        <Post content="This is a test post!" />
+        <Post content="This is another test post!" />
+    
+    </div>
+  )
 }
-
-export default App;
+export default App
